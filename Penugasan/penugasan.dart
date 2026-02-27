c89729d73c149734ba4c18ec2df0975444bbb71c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-02-27 14:26:56 +0700

    feat: penugasan

---
+++ Penugasan/penugasan.dart
@@ -0,0 +1,20 @@
+void main() {
+  // int a = 27;
+
+  // int a = 27;
+  //   a += 9;
+
+  // int a = 27;
+  //   a -= 9;
+
+  // int a = 27;
+  //   a *= 9;
+
+  // double a = 27;
+  // a /= 9;
+
+  int a = 27;
+  a ~/= 9;
+
+  print(a);
+}
