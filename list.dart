c99decae9d37eee330f181ca0266ab1e3e64fb55
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-02-27 13:45:17 +0700

    feat: add list

---
+++ list.dart
@@ -0,0 +1,9 @@
+void main(){
+  List<String> makanan = ["Nasi Goreng", "Sate", "Bakso"];
+  // print(makanan[0]); //akses elemen pertama
+  // makanan.add("Soto");//tambah elemen baru
+  // makanan.remove("Sate");// ngehapus elemen
+  makanan[1] = "Mie Ayam"; //ubah elemen kedua
+
+  print(makanan);
+}
